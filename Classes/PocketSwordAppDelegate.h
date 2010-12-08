5d988b5ca9156c1cc9fb9b198179b5a5859e6bd1
niccarter <devnull@localhost>
2010-12-08 11:32:50 +1100

    fixed bug: launching PS from another app & requesting a module list now works again...

--- Classes/PocketSwordAppDelegate.h
+++ Classes/PocketSwordAppDelegate.h
@@ -30,7 +30,7 @@
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
 //@property (nonatomic, retain) IBOutlet UITabBarController *tabBarController;
-@property (nonatomic, retain) IBOutlet NSURL *urlToOpen;
+@property (nonatomic, retain) NSURL *urlToOpen;
 
 + (PocketSwordAppDelegate *)sharedAppDelegate;
 
