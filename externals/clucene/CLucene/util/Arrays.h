ffff3e4f42b001db518e54a7c6cbc2bdfb283cea
niccarter <niccarter@mac.com>
2013-05-13 17:07:33 +1000

    fixing warnings...

--- externals/clucene/CLucene/util/Arrays.h
+++ externals/clucene/CLucene/util/Arrays.h
@@ -151,7 +151,7 @@ CL_NS_DEF(util)
 
 			_itr1 itr1 = val1->begin();
 			_itr2 itr2 = val2->begin();
-			while ( --size >= 0 ){
+			while ( --size > 0 ){
 				if ( !comp(*itr1,*itr2) )
 					return false;
 				itr1++;
