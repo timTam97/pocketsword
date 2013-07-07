e4827a4ad8607b999d94dc3fdb03a526ac61d858
niccarter <niccarter@mac.com>
2013-07-08 09:43:31 +1000

    updated forgotten about changes to strings files. Re-applied "Samuel" fix to Arabic l10n & updated locales.d.zip

--- Classes/PSLaunchViewController.m
+++ Classes/PSLaunchViewController.m
@@ -12,7 +12,7 @@
 #import "ZipArchive.h"
 #import "PSResizing.h"
 
-#define LOCALES_VERSION					@"loadedSWORDLocales-130613"
+#define LOCALES_VERSION					@"loadedSWORDLocales-130708"
 #define STRONGS_REAL_GREEK_VERSION		@"loadedBundledStrongsRealGreek-v1.4-121223"
 
 @implementation PSLaunchViewController
